7cd2f759e19297b18c35e4217a609a3908886744
newt <newtykins@outlook.com>
2020-12-27 01:09:02 +0000

    Make compatible with vercel

--- api/index.js
+++ api/index.js
@@ -1,10 +1,25 @@
+const app = require('express')();
 const axios = require('axios');
+const ms = require('ms');
+
 const spotify = new(require('node-spotify-api'))({
     id: process.env.SPOTIFYID,
     secret: process.env.SPOTIFYSECRET
 });
 
-module.export = async (_req, res) => {
+app.use(require('cors')()); // cors middleware
+app.use(require('morgan')('dev')); // morgan logger middleware
+
+const formatNumber = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+// osu
+app.get('/api/osu', async (_req, res) => {
+    const data = (await axios.get(`https://osu.ppy.sh/api/get_user?k=${process.env.OSU}&u=16009610`)).data;
+    res.send({ rank: formatNumber(data[0].pp_rank) });
+});
+
+// scrobbling
+app.get('/api/song', async (_req, res) => {
     const recentTrack = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=itsnewt&api_key=${process.env.LASTFM}&format=json&limit=1`)
         .then(res => res.data.recenttracks.track[0]);
     const spotifyTrack = (await spotify.search({ type: 'track', query: `${recentTrack.artist['#text']} - ${recentTrack.name}`})).tracks.items[0];
@@ -40,4 +55,7 @@ module.export = async (_req, res) => {
     } else {
         res.send({ message: 'newt is not listening to anything at the moment!' });
     }
-}
+});
+
+// start server
+app.listen('8080', () => console.log('Server started!'));
--- api/osu.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios');
-
-module.exports = async (_req, res) => {
-    const data = (await axios.get(`https://osu.ppy.sh/api/get_user?k=${process.env.OSU}&u=16009610`)).data;
-    res.send({ rank: formatNumber(data[0].pp_rank) });
-};
